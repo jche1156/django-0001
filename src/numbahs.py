86466b1c6e4368be5651761858ec63e61ab45ea8
John Chen <john.hk.chen@gmail.com>
2023-11-10 22:20:27 +0000

    Tidy up codebase

--- src/numbahs.py
+++
@@ -1,7 +0,0 @@
-"""A simple example used to show off linter"""
-from typing import Iterable
-
-
-def sum_even_numbers(numbers: Iterable[int]) -> int:
-    """Return the sum of all even numbers in the iterable."""
-    return sum(num for num in numbers if num % 2 == 0)
